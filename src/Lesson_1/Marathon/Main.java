f08d48301041fb53df998b493aa818aa69095900
Alsoexistnot <76039531+Alsoexistnot@users.noreply.github.com>
2021-06-16 14:39:13 +0300

    Update Main.java

--- src/Lesson_1/Marathon/Main.java
+++ src/Lesson_1/Marathon/Main.java
@@ -14,24 +14,5 @@ public class Main {
         team.showResults();
         System.out.println();
         team.teamInfo();
-
-
-
-
-
-
-
-
-//        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
-//        Obstacle[] course = {new Cross(80), new Wall(2), new Wall(1), new Cross(120)};
-//        for (Competitor c : competitors) {
-//            for (Obstacle o : course) {
-//                o.doIt(c);
-//                if (!c.isOnDistance()) break;
-//            }
-//        }
-//        for (Competitor c : competitors) {
-//            c.info();
-//        }
     }
 }
