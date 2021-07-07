db070d32e9122d5fce74be5e163c24683e5088fc
Alsoexistnot <76039531+Alsoexistnot@users.noreply.github.com>
2021-07-07 18:07:39 +0300

    Add files via upload

---
+++ lesson_7/src/server/MainServ.java
@@ -0,0 +1,71 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class MainServ {
+    private Vector<ClientHandler> clients;
+
+    public MainServ() {
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            AuthService.connect();
+            server = new ServerSocket(8188);
+            System.out.println("Сервер запущен!");
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился!");
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o : clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    public void whisperMsg (String writer, String msg, String nickname){
+        for (ClientHandler o : clients){
+            if (o.getNick().equals(nickname)) {
+                try {
+                    o.out.writeUTF("[W] " + writer + ": " + msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    public Vector<ClientHandler> getClients() {
+        return clients;
+    }
+}
